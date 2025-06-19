de5aaf282c42e0dd3f6298f28d838085aff2a4ae
Devvrath Sharma <devvrath.sharma@zordial.com>
2025-06-19 15:11:41 +0530

    frd

--- app.py
+++ app.py
@@ -32,3 +32,19 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/vcard.html')
+def serve_vcard():
+    return send_from_directory('static', 'vcard.html')
+
+@app.route('/')
+def home():
+    return send_from_directory('static', 'login.html')  # or your index.html
+
+if __name__ == '__main__':
+    app.run()
